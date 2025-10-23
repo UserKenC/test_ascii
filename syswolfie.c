9c3f144100549c240778db33627756359dada0c7
Kenneth Cahyadi <kenenthcahyadi1@gmail.com>
2025-10-23 22:37:13 +0800

    jamit

--- syswolfie.c
+++ syswolfie.c
@@ -1,13 +1,17 @@
-// syswolfie.c – xv6 x86 version
+// syswolfie.c  — xv6 x86 version
 #include "types.h"
+#include "x86.h"
 #include "defs.h"
+#include "date.h"
 #include "param.h"
+#include "memlayout.h"
 #include "mmu.h"
 #include "proc.h"
-#include "x86.h"
-#include "memlayout.h"
 #include "spinlock.h"
 #include "string.h"
+
+// we must declare the current process pointer
+extern struct proc *proc;
 
 // syscall: int sys_wolfie(void *buf, uint size)
 int
@@ -16,13 +20,13 @@ sys_wolfie(void)
   char *buf;
   int size;
 
-  // Fetch syscall arguments
+  // Get user arguments
   if(argptr(0, &buf, 0) < 0)
     return -1;
   if(argint(1, &size) < 0)
     return -1;
 
-  static const char *art =
+  static char *art =
     "          .--.\n"
     "         / _.-'\n"
     "    _ .-'/;\n"
@@ -38,9 +42,9 @@ sys_wolfie(void)
   if(size < len)
     return -1;
 
-  // Copy ASCII art to user space
-  if(copyout(proc->pgdir, (uint)buf, art, len) < 0)
+  // Copy art string into user buffer
+  if(copyout(proc->pgdir, (uint)buf, (char *)art, len) < 0)
     return -1;
 
-  return len; // return number of bytes copied
+  return len;
 }
