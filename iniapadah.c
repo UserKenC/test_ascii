88c6de70294a9393010612ed978790323b595589
Kenneth Cahyadi <kenenthcahyadi1@gmail.com>
2025-11-13 22:16:23 +0800

    WHATS GOING ON

--- iniapadah.c
+++ iniapadah.c
@@ -1,4 +1,1 @@
-_user/%: user/%.c
-	$(CC) $(CFLAGS) -fno-pic -static -fno-builtin -fno-stack-protector \
-	-Wall -Wextra -Wno-unused-parameter -m32 -MD -ggdb -I. -Iuser \
-	-o $@ $<
+echo -e '_user/%: user/%.c\n\t$(CC) $(CFLAGS) -fno-pic -static -fno-builtin -fno-stack-protector -Wall -Wextra -Wno-unused-parameter -m32 -MD -ggdb -I. -Iuser -o $@ $<' >> Makefile
