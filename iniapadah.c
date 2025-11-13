c32159094bf184a47f78825fb064a31ff3f2f69b
Kenneth Cahyadi <kenenthcahyadi1@gmail.com>
2025-11-13 21:29:34 +0800

    lab4 last comit kekny

--- iniapadah.c
+++ iniapadah.c
@@ -1,21 +1,14 @@
-int
-set_priority(int pid, int new_priority)
-{
-    struct proc *p;
+#include "types.h"
+#include "user.h"
 
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-        if(p->pid == pid){
-            if(new_priority < 0) new_priority = 0;
-            if(new_priority > 31) new_priority = 31;
-            p->priority = new_priority;
-            release(&ptable.lock);
+int main() {
+    int pid = getpid();
+    printf(1, "PID %d: setting high priority 25\n", pid);
+    set_priority(pid, 25);  // set high priority
 
-            // preempt if necessary
-            yield();  // voluntarily give up CPU if new priority < others
-            return 0;
-        }
+    for(int i = 0; i < 100; i++){
+        printf(1, "PID %d counting %d\n", pid, i);
     }
-    release(&ptable.lock);
-    return -1; // pid not found
+
+    exit();
 }
